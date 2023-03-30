b27af8108209f0086e15473745f9cb83d7b4c1f7
monomomo28 <128379237+monomomo28@users.noreply.github.com>
2023-03-30 18:57:13 +0900

    Update settings.js

--- theme/src/config/settings.js
+++ theme/src/config/settings.js
@@ -1,5 +1,5 @@
 export default {
-	title: `Odyssey Astro Theme | A Marketing Website Theme for Startups and Businesses`,
+	title: `unpacked event HP | HOME`,
 	description: `A simple, clean, and modern theme for a startup or businesses' marketing website.`,
 	url: `https://odyssey-theme.littlesticks.dev`, // No trailing slash!
 	name: `unpacked`, // The short name of the business or brand name. Used for things like the copyright in the footer.
